9f166fe0eee148108861105c423aff6817ad9151
lole <loganboganfarfignogan@gmail.com>
2023-06-05 14:01:26 -0500

    fixed the the premote command thing so it has more stuffs

--- pluginTest/src/main/java/woks/woks/matthew/permote.java
+++ pluginTest/src/main/java/woks/woks/matthew/permote.java
@@ -1,5 +1,6 @@
 package woks.woks.matthew;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,6 +10,7 @@ import woks.woks.CommandBase;
 import woks.woks.Msg;
 
 import java.util.List;
+import java.util.Objects;
 
 import static woks.woks.WOKS.Ranks;
 import static woks.woks.WOKS._namespacedKeyNumberRank;
@@ -22,16 +24,30 @@ public class permote {
                 if (player.isOp() || player.getName().equals("ShortPuppy14484")) {
                     PersistentDataContainer dataContainer = player.getPersistentDataContainer();
                     Integer role_rank_air_number = dataContainer.get(_namespacedKeyNumberRank, PersistentDataType.INTEGER);
+
+                    //could probley do some assert thing where the thing returns in the length cus other wise this is a fing mess
                     if (arguments.length == 0) {
                         // check if they have max rank
-                        if (role_rank_air_number >= Ranks.length) {
-                            Msg.send(sender, "You have reached max rank.");
+                        premote(sender, role_rank_air_number, dataContainer, 1, "+");
+                    } else if (arguments.length == 1) {
+                        Player player2 = Bukkit.getPlayer(arguments[0]);
+                        premote(player2, role_rank_air_number, dataContainer, 1, "+");
+                    } else if (arguments.length == 2) {
+                        Player player2 = Bukkit.getPlayer(arguments[0]);
+                        if (arguments[1].length() == 1) {
+                            premote(player2, role_rank_air_number, dataContainer, 1, arguments[1]);
                         } else {
-                            dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, role_rank_air_number + 1);
-                            Msg.send(sender, "Your rank is now " + (role_rank_air_number + 1));
+                            Msg.send(sender, "You need to put in a valid operater, must be '=', '+', '-', or '*'.");
+                        }
+                    } else if (arguments.length == 3) {
+                        Player player2 = Bukkit.getPlayer(arguments[0]);
+                        if (arguments[1].length() == 1) {
+                            premote(player2, role_rank_air_number, dataContainer, Integer.valueOf(arguments[2]), arguments[1]);
+                        } else {
+                            Msg.send(sender, "You need to put in a valid operater, must be '=', '+', '-', or '*'.");
                         }
                     } else {
-                        Msg.send(sender, "This has yet to be made. LOL.");
+                        Msg.send(sender, "This has yet to be made. LOL. Or you failed at life.");
                     }
                 } else {
                     Msg.send(player, "You need to be OP to ues this Command.");
@@ -39,15 +55,44 @@ public class permote {
                 return true;
             }
 
+
             @Override
             public String getUsage() {
-                return "/permote <playerName> <add|sub|set> <int:amount>";
+                return "/permote <playerName> <char:+|-|*|=> <int:amount>";
             }
 
             @Override
             public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
                 return null;
             }
-        }.enableDelay(2);
+        };
+
+
+    }
+
+    public void premote(CommandSender sender, Integer role_rank_air_number, PersistentDataContainer dataContainer, Integer amount, String op) {
+        if (role_rank_air_number >= Ranks.length - 1) {
+            Msg.send(sender, "You have reached max rank.");
+        } else if (Objects.equals(op, "-")) {
+            dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, role_rank_air_number - amount);
+            Msg.send(sender, "Your rank is now " + (role_rank_air_number - amount));
+        } else if (Objects.equals(op, "=")) {
+            dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, amount);
+            Msg.send(sender, "Your rank is now " + (amount));
+        } else if (Objects.equals(op, "+")) {
+            dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, role_rank_air_number + amount);
+            Msg.send(sender, "Your rank is now " + (role_rank_air_number + amount));
+        } else if (Objects.equals(op, "*")) {
+            if (role_rank_air_number * amount >= Ranks.length) {
+                dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, Ranks.length - 1);
+                Msg.send(sender, "Your rank is now " + (Ranks.length - 1));
+            } else {
+                dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, role_rank_air_number * amount);
+                Msg.send(sender, "Your rank is now " + (role_rank_air_number * amount));
+            }
+        } else {
+            dataContainer.set(_namespacedKeyNumberRank, PersistentDataType.INTEGER, role_rank_air_number + 1);
+            Msg.send(sender, "Your rank is now " + (role_rank_air_number + 1));
+        }
     }
 }
